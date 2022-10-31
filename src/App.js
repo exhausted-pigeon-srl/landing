959a4e08f6ba02b4374ef498026d754dd50ca5cd
DrGorilla_md <83670532+drgorillamd@users.noreply.github.com>
2022-10-31 21:22:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,53 +11,15 @@ function App() {
           <img src="./xp_webTransp.png" className="banner" alt="logo" />
         </p>
         <div className="box">
-          <!-- <div className="box-header">We provide blockchain solutions</div> -->
-
           <div className="box-body">
  <p>Exhausted Pigeon provides smart contract engineering and solutions for every part of your web3 stack.
               <br />
               Our main focus are EVM-compatible chains - we use Solidity, Vyper, Yul(p),
               Huff, or even raw bytecode - and Starknet.
               <br /><a href="mailto: info@exhausted-pigeon.xyz">Contact</a></p>
-            <!--<br /> Mint our business card (free NFT of a fully onchain svg, in
-            Yul): Soon -->
           </div>
         </div>
       </div>
-  <!--
-      <div className="textarea">
-        <div className="box">
-          <div className="box-body">
-            <Accordion flush>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>
-                  What are your main languages?
-                </Accordion.Header>
-                <Accordion.Body>
-                  Solidity, Cairo, Yul, Huff, Vyper
-                  <br /> JS/TS, python
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>
-                  What project are you currently working on?
-                </Accordion.Header>
-                <Accordion.Body>
-                  We are contributing, on a long term basis, to Juicebox and
-                  Defi Wonderland
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>What are you holding?</Accordion.Header>
-                <Accordion.Body>
-                  We will soon reflect our onchain balance sheet, live
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-        </div>
-      </div>
-                  -->
     </>
   );
 }
