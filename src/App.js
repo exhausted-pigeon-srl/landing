95541f607c994fc51f6fefbef4d85933e0b559a9
DrGorilla_md <dr_gorilla@protonmail.com>
2022-10-31 10:31:28 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import Price from "./Price.js";
+import Profile from "./Profile.js";
 import Accordion from "react-bootstrap/Accordion";
 import "./App.css";
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
+
+const client = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+});
 
 function App() {
   return (
@@ -11,12 +18,25 @@ function App() {
           <img src="./xp_webTransp.png" className="banner" alt="logo" />
         </p>
         <div className="box">
-          <div className="box-header">
-            <Price />
-          </div>
+          <div className="box-header">FAQ</div>
 
           <div className="box-body">
-            <p> placeholder for the cool thing </p>
+            <p>
+              We are a company specializing in blockchain developement. While
+              our expertise is centered on onchain solutions, we provide
+              solutions for every part of your web3 stack.
+              <br />
+              Our main focus are EVM-compatible chains (Solidity, Vyper, Yul(p),
+              Huff, even raw bytecode) and Starknet - we are obviously always
+              open to challenges in new vm.
+              <br />
+              Feel free to mint our fully onchain adaptive svg NFT Business card
+              here, available on Arbitrum, Polygon and mainnet:
+              <br />
+              <WagmiConfig client={client}>
+                <Profile />
+              </WagmiConfig>
+            </p>
           </div>
         </div>
       </div>
@@ -26,11 +46,11 @@ function App() {
           <div className="box-body">
             <Accordion flush>
               <Accordion.Item eventKey="0">
-                <Accordion.Header> ?</Accordion.Header>
-                <Accordion.Body>foo</Accordion.Body>
+                <Accordion.Header>?</Accordion.Header>
+                <Accordion.Body></Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="1">
-                <Accordion.Header> ?</Accordion.Header>
+                <Accordion.Header></Accordion.Header>
                 <Accordion.Body>foo</Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="2">
